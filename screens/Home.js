565a508b2173398e73764ca7822513f644c504c7
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2018-12-25 13:10:38 +0700

    Menampilkan dengan ScrollView dan FlatList

--- screens/Home.js
+++ screens/Home.js
@@ -1,106 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
-import axios from 'axios'
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
+import axios from 'axios';
 
 
-export default class App extends React.Component {
+export default class Home extends React.Component {
   static navigationOptions = {
     title : 'Home'
   }
-  constructor(props){
-    super(props)
+  constructor(){
+    super()
     this.state = {
-      hasil: 0,
-      input: '',
-      yesno: '',
-      showAbout: false
+      planets: []
     }
-  }
-
-  input = (val) => {
-    const {input} = this.state;
-    const inputValue = val;
-
-    this.setState({input: inputValue})
-    const hasil = this.hasilPerhitungan(inputValue)
-    this.setState({hasil: hasil})
-  }
-
-  hasilPerhitungan = (inputValue) => {
-    const arrayInput = inputValue.split("");
-    let hasil = 0;
-    let angka1 = "";
-    let angka2 = "";
-    let operator = "";
-
-    for (var i = 0; i < arrayInput.length; i++) {
-      switch (arrayInput[i]){
-        case "+": 
-          operator = "+"; 
-          angka1 = hasil; 
-          angka2 = ""; 
-        break;
-        case "-": 
-          operator = "-"; 
-          angka1 = hasil; 
-          angka2 = ""; 
-        break;
-        case "*": 
-          operator = "*"; 
-          angka1 = hasil; 
-          angka2 = ""; 
-        break;
-        case "/": 
-          operator = "/"; 
-          angka1 = hasil; 
-          angka2 = ""; 
-        break;
-        default:
-          arrayInput[i] == " " ? angka2 = "" : angka2 += String(arrayInput[i])
-          angka1 == "" ? angka1 = angka2 : hasil;
-          if (arrayInput[i] != " ") {
-            switch(operator){                    
-              case "+": 
-                hasil = 
-                Number(angka1) + Number(angka2); 
-              break;
-              case "-": 
-                hasil = Number(angka1) - Number(angka2); 
-              break;
-              case "*": 
-                hasil = Number(angka1) * Number(angka2); 
-              break;
-              case "/": 
-                hasil = Number(angka1) / Number(angka2); 
-              break;
-              default: 
-                hasil = Number(angka1) + 0;
-          }
-        }      
-      }    
-    }
-    return hasil;
   }
 
 componentDidMount(){
-  axios.get('https://yesno.wtf/api').then((res) => this.setState({yesno: res.data})).catch(err => console.log(err))
+  axios.get('https://swapi.co/api/planets').then((res) => this.setState({planets: res.data.results})).catch(err => console.log(err))
 }
 
 
   render() {
-    const { showAbout, yesno } = this.state
   
     return (
       <View style={styles.container}>
-        <Text style={{fontSize: 32}}>Kalkulator</Text>
-        <TextInput 
-          style={styles.inputBox} 
-          value={this.state.input} 
-          onChangeText={(val) => this.input(val)} 
+      {/* dirender 1 kali 
+          scrollview membuat halaman yang banyak bagian foto nama deskripsi / tidak banyak data nya
+      */}
+        {/* <ScrollView>
+          {
+            this.state.planets.map( planet => {
+              return <Text style={{fontSize: 50}}>{planet.name}</Text>
+            })
+          }
+        </ScrollView> */}
+
+        {/* dirender sesuai dia berada 
+            flat list digunakan data saat banyak
+        */}
+        <FlatList 
+          data={this.state.planets}
+          renderItem={({item}) => {
+            return <Text>{item.name}</Text>
+          }}
         />
-        <Text style={ {fontSize: 25} }>Hasil : {this.state.hasil}</Text>
-        <Image source={{uri: yesno.image}} style={{width: 100, height:100}} />
-        <Text style={ {fontSize: 25} }>{yesno.answer}</Text>
       </View>
     );
   }
